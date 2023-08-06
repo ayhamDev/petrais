3672ca580a07050d77fda1092074902343b9f741
ayham <ayhamgm81@gmail.com>
2023-08-06 11:54:18 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: "#FA2525",
+      secondary: "#EEEEEE",
+      offblack: "#111111",
+    },
+    fontSize: {
+      // Desktop (default)
+      sm: "0.9375rem",
+      base: "1.25rem",
+      md: "1.5625rem",
+      lg: "2.1875rem",
+      xl: "3.75rem",
+      // Mobile
+      "base-mobile": "1.0625rem",
+      "md-mobile": "1.25rem",
+      "lg-mobile": "1.5625rem",
+      "xl-mobile": "2.1875rem",
+    },
+    borderRadius: {
+      primary: "30px 2px 30px 30px",
+      secondary: "40px",
+      offshape: "30px 2px 30px 2px",
+      sm: "10px",
     },
   },
   plugins: [],
-}
+};
