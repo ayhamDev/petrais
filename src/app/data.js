98da5c89aa496b2f392bd0edab20dd23b4f3faa0
ayham <ayhamgm81@gmail.com>
2023-08-08 00:05:08 +0300

    fixed small bug 2

--- src/app/data.js
+++ src/app/data.js
@@ -1,6 +1,6 @@
 const services = [
   {
-    name: "Design Services",
+    name: "design services",
     description: `Lorem ipsum dolor sit amet consectetur. Ac netus lorem suspendisse sit lectus dolor. Volutpat in blandit nibh eget. At dignissim dignissim ut nunc pharetra pellentesque aenean. Mattis lobortis sodales consequat fermentum. Convallis et viverra morbi eget in integer. Maecenas pellentesque eget at vitae quis. Lorem pellentesque arcu justo ut diam tortor vitae magnis cras. Tincidunt sed id sed sit morbi odio lorem. Montes dui aliquet eleifend vulputate posuere neque commodo fames elementum. Nisi eget at nunc et pellentesque. In varius lacus.`,
     icon: () => (
       <svg
@@ -162,7 +162,7 @@ const services = [
     ),
   },
   {
-    name: "Security Services",
+    name: "Security services",
     description: `Lorem ipsum dolor sit amet consectetur. Ac netus lorem suspendisse sit lectus dolor. Volutpat in blandit nibh eget. At dignissim dignissim ut nunc pharetra pellentesque aenean. Mattis lobortis sodales consequat fermentum. Convallis et viverra morbi eget in integer. Maecenas pellentesque eget at vitae quis. Lorem pellentesque arcu justo ut diam tortor vitae magnis cras. Tincidunt sed id sed sit morbi odio lorem. Montes dui aliquet eleifend vulputate posuere neque commodo fames elementum. Nisi eget at nunc et pellentesque. In varius lacus.`,
     icon: () => (
       <svg
