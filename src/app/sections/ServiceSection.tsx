cbfb3336b5e24363dce10be29bbabb45c3f54204
ayham <ayhamgm81@gmail.com>
2023-08-08 13:54:28 +0300

    added links to services section

--- src/app/sections/ServiceSection.tsx
+++ src/app/sections/ServiceSection.tsx
@@ -1,6 +1,7 @@
 import Wrapper from "@/components/Wrapper";
 import Typography from "@/components/Typography";
 import { services } from "@/data";
+import Link from "next/link";
 
 export default function ServiceContainer() {
   return (
@@ -8,20 +9,19 @@ export default function ServiceContainer() {
       <div className="flex lg:justify-between justify-center flex-wrap gap-6 ">
         {services.map((service, index) => {
           return (
-            <div
-              key={index}
-              className="w-[200px] h-[250px] border-2 border-primary rounded-secondary transition-all duration-300 cursor-pointer hover:scale-105 hover:shadow-xl hover:bg-primary group flex flex-col select-none"
-            >
-              <div className="m-auto mt-8 w-fit flex-1">
-                <service.icon />
+            <Link key={index} href={`/services/${service.link}`}>
+              <div className="w-[200px] h-[250px] border-2 border-primary rounded-secondary transition-all duration-300 cursor-pointer hover:scale-105 hover:shadow-xl hover:bg-primary group flex flex-col select-none">
+                <div className="m-auto mt-8 w-fit flex-1">
+                  <service.icon />
+                </div>
+                <Typography
+                  type="body1"
+                  className="text-center text-primary mt-4 flex-1 group-hover:text-white transition-all duration-300"
+                >
+                  {service.name}
+                </Typography>
               </div>
-              <Typography
-                type="body1"
-                className="text-center text-primary mt-4 flex-1 group-hover:text-white transition-all duration-300"
-              >
-                {service.name}
-              </Typography>
-            </div>
+            </Link>
           );
         })}
       </div>
